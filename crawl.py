13c815629649a8b9bc0fedf23cb4bcb364b6a71b
Allohn <allencris963@gmail.com>
2025-08-18 15:37:39 +0200

    Commit didn't work

--- crawl.py
+++ crawl.py
@@ -1,3 +1,6 @@
+from urllib.parse import urlparse
+
+
 def normalize_url(input_url: str) -> str:
     """
     Check an input URL (string) an normalize it into some standard format. In our case, we will remove the https:// at the beginning of the url, and the / at the end.
@@ -6,7 +9,29 @@ def normalize_url(input_url: str) -> str:
         input_url (str): An arbitrary url.
 
     Returns:
-        normalized_url (str): The same url but normalized to our convention"""
-    normalized_url = input_url
+        normalized_url (str): The same url but normalized to our convention
+    """
+    # Suite of basic input checks
+    if not isinstance(input_url, str):
+        raise TypeError("Expected a string input")
+    if input_url == "":
+        raise ValueError("An empty string is not a url")
 
+    # Store the url as its separate parts in url_object
+    url_object = urlparse(input_url)
+
+    # Perform some clean-up to return it in our wanted format (netloc/path)
+    netloc = (url_object.netloc).lower()
+    path = (url_object.path).lower()
+    # Knock off the trailing / on the path if it exists
+    split_path = list(path)
+
+    if path == "/":
+        path = ""
+
+    elif split_path[-1] == "/":
+        split_path.pop(-1)
+        path = "".join(split_path)
+
+    normalized_url = netloc + path
     return normalized_url
--- test_crawl.py
+++ test_crawl.py
@@ -29,8 +29,17 @@ class TestCrawl(unittest.TestCase):
         input_url = "https://boot.dev/Path"
 
         actual_output = normalize_url(input_url)
-        expected_output = "blog.boot.dev/path"
+        expected_output = "boot.dev/path"
         self.assertEqual(actual_output, expected_output)
+
+    def test_empty_string(self):
+        with self.assertRaises(ValueError):
+            normalize_url("")
+
+    def test_wrong_type_input(self):
+        for bad_input in [123, None, 3.14, ["list"], {"dict": "value"}]:
+            with self.assertRaises(TypeError):
+                normalize_url(bad_input)
 
 
 if __name__ == "__main__":
