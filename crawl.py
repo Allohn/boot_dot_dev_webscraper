7c715a435f72ccad958d0a1cbc7be12d7ad5fd56
Allohn <allencris963@gmail.com>
2025-08-19 15:54:01 +0200

    Finished function to take in a html string and base_url, and scan the html to pick out and return a list of urls on that page. Implemented a suite of unit tests for the function

--- crawl.py
+++ crawl.py
@@ -1,4 +1,15 @@
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup
+
+# Sample page source
+sample_html = """<!DOCTYPE HTML><html><body lang="en" dir="ltr"><div id="wrapper" class="hfeed">
+<div id="header">
+    <ul id="mothership">
+        <li> <a href="http://www.ubuntu.com/partners">Partners</a> </li>
+        <li> <a href="http://www.ubuntu.com/support">Support</a> </li>
+        <li> <a href="http://www.ubuntu.com/community">Community</a> </li>
+        <li> <a href="http://www.ubuntu.com">Ubuntu.com</a> </li>
+    </ul></body></html>"""
 
 
 def normalize_url(input_url: str) -> str:
@@ -26,12 +37,68 @@ def normalize_url(input_url: str) -> str:
     # Knock off the trailing / on the path if it exists
     split_path = list(path)
 
-    if path == "/":
+    if path == "/":  # Check for an empty path/trailing slash of any kind, and remove it
         path = ""
 
-    elif split_path[-1] == "/":
+    elif (
+        split_path[-1] == "/"
+    ):  # Check the characters in the path and knock off the last one if it's a trailing slash. Join back together
         split_path.pop(-1)
         path = "".join(split_path)
 
     normalized_url = netloc + path
     return normalized_url
+
+
+def get_urls_from_html(html: str, base_url: str) -> list[str]:
+    """
+    Take in a HTML string, and a root url, and crawl the html to find links. Use the root_url to produce a list of absolute urls connected to the page
+
+    Args:
+        html (str): A html string
+        base_url (str): An absolute url that is the root url of all the links on the page
+
+    Returns:
+        url_list list[str]: A list of un-normalized absolute urls.
+    """
+
+    # Suite of basic input checks
+    if not (isinstance(base_url, str) and (isinstance(html, str))):
+        raise TypeError("Expected a string input")
+    if not base_url or not html:
+        raise ValueError("An empty string is not a url")
+
+    soup = BeautifulSoup(html, "lxml")  # Initialize the BS object with the lxml parser
+
+    # Make a list of hrefs
+    anchor_type = ["a", "A"]
+    href_list = []
+
+    for (
+        a_type
+    ) in (
+        anchor_type
+    ):  # For loop to make a check on both <a> and <A> in case of capitalization problems.
+        anchor_list = soup.find_all(a_type)  # List of all anchor blocks
+        for link in anchor_list:
+            href_list.append(link.get("href"))
+
+    # Check to make sure everything is absolute
+    absolute_url_list = []
+    for href in href_list:
+        url_object = urlparse(href)
+
+        # Split up the url into the network location and the path
+        scheme = url_object.scheme
+        netloc = url_object.netloc
+        path = url_object.path
+
+        if netloc == "":
+            absolute_url_list.append(urljoin(base_url, path))
+        else:
+            absolute_url_list.append(scheme + "://" + netloc + path)
+
+    # Check for duplicates
+    absolute_url_list = list(set(absolute_url_list))
+
+    return absolute_url_list
--- test_crawl.py
+++ test_crawl.py
@@ -1,9 +1,10 @@
 import unittest
-from crawl import normalize_url
+from crawl import normalize_url, get_urls_from_html
 
 
 class TestCrawl(unittest.TestCase):
-    def test_strip_scheme_and_trailing_slashes_normalize_url(self):
+    # Tests for the normalize_url() function in crawl.py
+    def test_NORMALIZE_URL_base_functionality(self):
         input_url = "https://blog.boot.dev/path/"
 
         actual_output = normalize_url(input_url)
@@ -11,35 +12,93 @@ class TestCrawl(unittest.TestCase):
 
         self.assertEqual(actual_output, expected_output)
 
-    def test_no_path(self):
+    def test_NORMALIZE_URL_no_path(self):
         input_url = "https://blog.boot.dev/"
 
         actual_output = normalize_url(input_url)
         expected_output = "blog.boot.dev"
         self.assertEqual(actual_output, expected_output)
 
-    def test_uppercase_domain(self):
+    def test_NORMALIZE_URL_uppercase_domain(self):
         input_url = "https://blog.BOOT.dev/Path"
 
         actual_output = normalize_url(input_url)
         expected_output = "blog.boot.dev/path"
         self.assertEqual(actual_output, expected_output)
 
-    def test_no_prefix(self):
+    def test_NORMALIZE_URL_no_prefix(self):
         input_url = "https://boot.dev/Path"
 
         actual_output = normalize_url(input_url)
         expected_output = "boot.dev/path"
         self.assertEqual(actual_output, expected_output)
 
-    def test_empty_string(self):
+    def test_NORMALIZE_URL_empty_string(self):
         with self.assertRaises(ValueError):
             normalize_url("")
 
-    def test_wrong_type_input(self):
+    def test_NORMALIZE_URL_wrong_type_input(self):
         for bad_input in [123, None, 3.14, ["list"], {"dict": "value"}]:
             with self.assertRaises(TypeError):
                 normalize_url(bad_input)
+
+    # Tests for the get_urls_from_html function
+    def test_GET_URLS_FROM_HTML_base_functionality(self):
+        input_url = "http://www.ubuntu.com"
+        sample_html = """<!DOCTYPE HTML><html><body lang="en" dir="ltr"><div id="wrapper" class="hfeed">
+        <div id="header">
+            <ul id="mothership">
+                <li> <a href="/partners">Partners</a> </li>
+                <li> <a href="http://www.ubuntu.com/partners">Partners</a> </li>
+                <li> <a href="http://www.ubuntu.com/support">Support</a> </li>
+                <li> <a href="http://www.ubuntu.com/community">Community</a> </li>
+                <li> <a href="http://www.ubuntu.com">Ubuntu.com</a> </li>
+            </ul></body></html>"""
+
+        actual_output = get_urls_from_html(sample_html, input_url)
+        expected_output = [
+            "http://www.ubuntu.com/partners",
+            "http://www.ubuntu.com/support",
+            "http://www.ubuntu.com/community",
+            "http://www.ubuntu.com",
+        ]
+        self.assertEqual(set(actual_output), set(expected_output))
+
+    def test_GET_URLS_FROM_HTML_relative_urls_to_absolute_urls(self):
+        input_url = "https://blog.boot.dev"
+        input_body = "<html><body><a href='/partners'>Partners</a></body></html>"
+
+        actual_output = get_urls_from_html(input_body, input_url)
+        expected_output = ["https://blog.boot.dev/partners"]
+        self.assertEqual(actual_output, expected_output)
+
+    def test_GET_URLS_FROM_HTML_repeated_urls(self):
+        input_url = "https://blog.boot.dev"
+        input_body = "<html><body><a href='https://blog.boot.dev'><span>Boot.dev</span></a><a href='https://blog.boot.dev'><span>Boot.dev</span></a></body></html>"
+
+        actual_output = get_urls_from_html(input_body, input_url)
+        expected_output = ["https://blog.boot.dev"]
+        self.assertEqual(actual_output, expected_output)
+
+    def test_GET_URLS_FROM_HTML_case_sensitivity(self):
+        input_url = "https://blog.boot.dev"
+        input_body = "<html><body><A href='https://blog.boot.dev'><span>Boot.dev</span></a></body></html>"
+
+        actual_output = get_urls_from_html(input_body, input_url)
+        expected_output = ["https://blog.boot.dev"]
+        self.assertEqual(actual_output, expected_output)
+
+    def test_GET_URLS_FROM_HTML_empty_string(self):
+        input_url = "https://blog.boot.dev"
+
+        with self.assertRaises(ValueError):
+            get_urls_from_html("", input_url)
+
+    def test_GET_URLS_FROM_HTML_wrong_type_input(self):
+        for bad_input1 in [123, None, 3.14, ["list"], {"dict": "value"}]:
+            for bad_input2 in [123, None, 3.14, ["list"], {"dict": "value"}]:
+                with self.assertRaises(TypeError):
+                    get_urls_from_html(bad_input1, bad_input2)
 
 
 if __name__ == "__main__":
