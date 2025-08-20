71c72b3953dd45057a23b409881920b9b8aa01eb
Allohn <allencris963@gmail.com>
2025-08-20 11:42:44 +0200

    Added CLI functionality to main

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import sys
+
+
 def main():
+    # Usage: uv run main.py base_url
+    CLI_input = sys.argv
+
+    if len(CLI_input) < 2:
+        print("no website provided")
+        sys.exit(1)
+    elif len(CLI_input) > 2:
+        print("too many arguments provided")
+        base_url = str(CLI_input[1])
+        sys.exit(1)
+    else:
+        base_url = str(CLI_input[1])
+        print(f"starting crawl of: {base_url}")
 
     print("Hello from babies-first-webscraper!")
 
